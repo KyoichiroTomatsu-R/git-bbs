4052053f6a3af7c5b04fb7c201bdd02161764683
Tomoki <84486284+tomoki-rks@users.noreply.github.com>
2021-06-04 14:19:23 +0900

    insertArticleControllerの完成

---
+++ src/main/java/com/example/controller/InsertArticleController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InsertArticleController {
+    @RequestMapping("/insertArticle")
+    public String index(@Validated ArticleForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "redirect:/";
+        }
+        Article article = new Article();
+        article.setName(form.getName());
+        article.setContent(form.getContent());
+        articleRepository.insert(article);
+        return "redirect:/";
+    }
+}
