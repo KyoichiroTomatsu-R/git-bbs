8da3ca6eb311a46002dd204690f7918fdf20a5f7
Tomoki <84486284+tomoki-rks@users.noreply.github.com>
2021-06-04 15:47:52 +0900

    Repositoryが作成されたので、Controllerで利用

--- src/main/java/com/example/controller/DeleteArticleController.java
+++ src/main/java/com/example/controller/DeleteArticleController.java
@@ -1,10 +1,16 @@
 package com.example.controller;
 
+import com.example.repository.ArticleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DeleteArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
     @RequestMapping("/deleteArticle")
     public String deleteArticle(int id) {
         articleRepository.deleteById(id);
