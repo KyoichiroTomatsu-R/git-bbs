942e5c190ccf70f8d7475669f90361511ce283c9
Tomoki <84486284+tomoki-rks@users.noreply.github.com>
2021-06-04 14:33:26 +0900

    InsertCommentControllerを作成

---
+++ src/main/java/com/example/controller/InsertCommentController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InsertCommentController {
+    @RequestMapping("/insertComment")
+    public String insertComment(@Validated CommentForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return index(model);
+        }
+        Comment comment = new Comment();
+        BeanUtils.copyProperties(form, comment);
+        // form側はStringなため、BeanUtilsでコピーされない？
+        comment.setArticleId(Integer.parseInt(form.getArticleId()));
+        commentRepository.insert(comment);
+        return "redirect:/";
+    }
+}
